f6757067431b2a685b50ec68173003dd5a2cf4f5
Roderick Kennedy <roderick.kennedy@gmail.com>
2025-04-11 19:38:14 +0100

    Initial commit for multi-port branch.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const teleport_server	= require('teleportxr')
 const client_manager	= require('teleportxr/client/client_manager');
 const scene				= require("teleportxr/scene/scene");
+const resources			= require("teleportxr/scene/resources");
 const open = require('open');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+
+const WebSocketServer = require("ws");
 
 // Create a scene, so we can fill it with stuff.
 var sc					= new scene.Scene();
@@ -33,30 +36,40 @@ function onClientPostCreate(clientID) {
 cm.SetClientPostCreationCallback(onClientPostCreate);
 
 // Having set up the callbacks, we start the server running.
-teleport_server.initServer();
+
+const wss=teleport_server.initServer();
+const dashboard_port = process.env.PORT || 9000;
 
 // Create a simple http server for scene management and display.
 // This will be accessible at localhost:9000 via a browser.
 // The dashboard uses the writeState functions of the teleport server classes
 // to send html summaries of their state to the dashboard.
-const express_app = express()
-const express_server = http.createServer(express_app);
-const express_io = socketIo(express_server);
-dashboard_port = process.env.PORT || 9000;
+const express_app = express();
+express_app.use(express.static('dashboard_public'));
+// Also serve any static 3D resources when requested.
+express_app.use(express.static('http_resources'));
+// Don't pass express_app to createServer - that would cause it to initalize before websockets is connected
+const http_server = express_app.listen(dashboard_port);
+// Also mount the app here
+http_server.on('upgrade', function upgrade(request, socket, head) {
+	const { pathname } = new URL(request.url, 'wss://base.url');
+	 wss.handleUpgrade(request, socket, head, function done(ws) {
+		wss.emit('connection', ws, request);
+  });
+});
+
+resources.Resource.SetDefaultPathRoot("http://localhost:9000")
 
 express_app.use(express.static('dashboard_public'));
-
+/*
 express_io.on('connection', (socket) => {
 	console.log('A dashboard client connected');
 	setInterval(() => {
 	  socket.emit('scene', sc.writeState());
 	  socket.emit('client_manager', cm.writeState());
 	}, 1000);
-});
+});*/
   
-express_server.listen(dashboard_port, () => {
-  console.log(`Dashboard server running on port ${dashboard_port}`);
-});
 
 // opens the url in the default browser if running locally (port 9000)
 if(dashboard_port==9000)
@@ -64,5 +77,5 @@ if(dashboard_port==9000)
 
 // Create a second http server, the Content Server. This is to 
 // Create a simple http server for scene management:
-const fileserver = require('./file-server.js');
-fileserver.startStaticFileServer("http_resources");
+////const fileserver = require('./file-server.js');
+//fileserver.startStaticFileServer("http_resources");
