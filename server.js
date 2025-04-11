6ffd58c459aa5170232101da32a38877035e722a
Roderick Kennedy <roderick.kennedy@gmail.com>
2025-04-11 22:19:23 +0100

    Use only a single http server, to avoid the need for multiple ports.

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ cm.SetClientPostCreationCallback(onClientPostCreate);
 
 // Having set up the callbacks, we start the server running.
 
+const signaling_port = process.env.PORT || 8081;
 const wss=teleport_server.initServer();
-const dashboard_port = process.env.PORT || 9000;
 
 // Create a simple http server for scene management and display.
 // This will be accessible at localhost:9000 via a browser.
@@ -49,7 +49,7 @@ express_app.use(express.static('dashboard_public'));
 // Also serve any static 3D resources when requested.
 express_app.use(express.static('http_resources'));
 // Don't pass express_app to createServer - that would cause it to initalize before websockets is connected
-const http_server = express_app.listen(dashboard_port);
+const http_server = express_app.listen(signaling_port);
 // Also mount the app here
 http_server.on('upgrade', function upgrade(request, socket, head) {
 	const { pathname } = new URL(request.url, 'wss://base.url');
@@ -58,7 +58,7 @@ http_server.on('upgrade', function upgrade(request, socket, head) {
   });
 });
 
-resources.Resource.SetDefaultPathRoot("http://localhost:9000")
+resources.Resource.SetDefaultPathRoot(`http://localhost:${signaling_port}`)
 
 express_app.use(express.static('dashboard_public'));
 /*
@@ -72,10 +72,5 @@ express_io.on('connection', (socket) => {
   
 
 // opens the url in the default browser if running locally (port 9000)
-if(dashboard_port==9000)
-	open('http://localhost:9000');
-
-// Create a second http server, the Content Server. This is to 
-// Create a simple http server for scene management:
-////const fileserver = require('./file-server.js');
-//fileserver.startStaticFileServer("http_resources");
+if(!process.env.PORT)
+	open(`http://localhost:${signaling_port}`);
